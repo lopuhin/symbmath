a07d90c9be8b315af89c03cdf171844a4773b421
lopuhin <kostia.lopuhin@gmail.com>
2010-11-03 10:39:23 +0300

    get strings in lexer, one symbol lookahead in parser

--- lexer.cpp
+++ lexer.cpp
@@ -7,7 +7,9 @@ typedef enum {VARIABLE, NUMBER, LPAREN, RPAREN,
 	      PLUS, MINUS, MULTIPLY, DIVIDE, POWER} Symbol;
 	      
 
-int lexer(const char* input, Symbol* result) {
+int lexer(const char* input, Symbol* result, char** result_strings) {
+  // return number of tokens. Fill result with tokens,
+  // and result_strings with corresponding strings
   const char pattern[] = "([_a-zA-Z]+[_a-zA-Z0-9]*|[0-9]+|\\+|\\-|\\*|\\^|\\(|\\))";
   regex_t re;
   regmatch_t pm;
@@ -17,7 +19,7 @@ int lexer(const char* input, Symbol* result) {
   }
   // this call to regexec() finds the first match on the line
   error = regexec(&re, input, 1, &pm, 0);
-  int pos = 0, token_index = 0;
+  int pos = 0, token_index = 0, match_len;
   Symbol token;
   char token_start;
   while (error == 0) {  // while matches found
@@ -38,14 +40,21 @@ int lexer(const char* input, Symbol* result) {
       else
 	token = VARIABLE;
     }
-    result[token_index++] = token;
+    match_len = pm.rm_eo - pm.rm_so;
+    char* s = new char[match_len + 1];
+    for(int i = pm.rm_so; i <= pm.rm_eo; i++ )
+      s[i - pm.rm_so] = input[pos + i];
+    s[match_len] = 0;
+    result_strings[token_index] = s;
+    result[token_index] = token;
+    token_index++;
     pos += pm.rm_eo;
     error = regexec(&re, input + pos, 1, &pm, REG_NOTBOL);
   }
   return token_index;
 }
 
-char* token_to_str(Symbol token) {
+const char* token_to_str(Symbol token) {
   switch(token) {
   case LPAREN: return "LPAREN";
   case RPAREN: return "RPAREN";
@@ -61,9 +70,10 @@ char* token_to_str(Symbol token) {
 
 void test_lexer(const char* input) {
   Symbol result[100];
-  int n_tokens = lexer(input, result);
+  char* strings[100];
+  int n_tokens = lexer(input, result, strings);
   for(int i = 0; i < n_tokens; i++) {
-    cout << token_to_str(result[i]) << " ";
+    cout << token_to_str(result[i]) << ": " << strings[i] << endl;
   }
   cout << endl;
 }
--- parser.cpp
+++ parser.cpp
@@ -4,6 +4,10 @@
 
 int pos;
 
+Symbol nextsym(Symbol* tokens) {
+  return tokens[pos];
+}
+
 Symbol getsym(Symbol* tokens) {
   cout << token_to_str(tokens[pos]) << " ";
   return tokens[pos++];
@@ -13,43 +17,51 @@ void assert(Symbol s, Symbol expected) {
   // TODO - raise Exception
 }
 
-void expression(Symbol* tokens);
+void expression(Symbol* tokens, char** strings);
 
-void factor(Symbol* tokens) {
-  Symbol s = getsym(tokens);
-  if (s == NUMBER || s == VARIABLE)
-    ;
-  else {
+void factor(Symbol* tokens, char** strings) {
+  Symbol s = nextsym(tokens);
+  if (s == NUMBER || s == VARIABLE) {
+    getsym(tokens);
+    
+  } else {
     assert(s, LPAREN);
-    expression(tokens);
+    expression(tokens, strings);
     assert(getsym(tokens), RPAREN);
   }
 }
 
-void term(Symbol* tokens) {
-  factor(tokens);
-  Symbol s = getsym(tokens);
-  if (s == DIVIDE || s == MULTIPLY)
-    factor(tokens);
+void term(Symbol* tokens, char** strings) {
+  factor(tokens, strings);
+  Symbol s = nextsym(tokens);
+  if (s == DIVIDE || s == MULTIPLY) {
+    getsym(tokens);
+    factor(tokens, strings);
+  }
 }
 
-void expression(Symbol* tokens) {
-  term(tokens);
-  Symbol s = getsym(tokens);
-  if (s == PLUS || s == MINUS)
-    term(tokens);
+void expression(Symbol* tokens, char** strings) {
+  term(tokens, strings);
+  Symbol s = nextsym(tokens);
+  if (s == PLUS || s == MINUS) {
+    getsym(tokens);
+    term(tokens, strings);
+  }
 }
 
 void parse(const char* input) {
-  Symbol* tokens = new Symbol[strlen(input)];
-  int n_tokens = lexer(input, tokens);
+  int max_tokens = strlen(input);
+  Symbol* tokens = new Symbol[max_tokens];
+  char** strings = new char*[max_tokens];
+  int n_tokens = lexer(input, tokens, strings);
   pos = 0;
-  expression(tokens);
-  cout << pos << " " << n_tokens << endl;
+  expression(tokens, strings);
+  cout << endl << pos << " " << n_tokens << endl;
 }
 
 
 int main() {
   parse("1 + 2 * 3");
+  parse("1 * 2 + 3");
   return 0;
 }
